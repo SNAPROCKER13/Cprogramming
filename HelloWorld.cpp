d6f4cecafb74f1a29dfaf2d0f81cc36123f02512
SNAPROCKER13 <theerawat_nap@hotmail.com>
2020-12-27 14:49:08 +0700

    frist commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello World !!!\n\n\n");
+	
+	printf("My firstname is THEERAWAT.\n");
+	printf("My surname is SOMSIN.\n");
+	printf("My nickname is SNAP\n");
+	
+	return 0;
+}
