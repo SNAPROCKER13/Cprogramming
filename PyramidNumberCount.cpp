164d942151c4914d42efe41a38722a7c299cf9c9
SNAPROCKER13 <67057675+SNAPROCKER13@users.noreply.github.com>
2020-12-28 22:54:29 +0700

    Update PyramidNumberCount.cpp

--- PyramidNumberCount.cpp
+++ PyramidNumberCount.cpp
@@ -2,21 +2,33 @@
 
 int main()
 {
-	int i,j,n,line = 1;
+	printf("===PyramidNumberCount===\n\n");
+	int i,j,n;
 	
-	scanf("%d",&n);
-	
-	for(i = 1;i <= n;i++)
+	while(1)
 	{
-		for(j = n;j >= i;j--)
+		int line = 1;
+		printf("Input Number : ");
+		scanf("%d",&n);
+	
+	printf("\n");
+	
+		for(i = 1;i <= n;i++)
 		{
-			printf(" ");
-		}
+			for(j = n;j >= i;j--)
+			{
+				printf(" ");
+			}
 		
-		for(j = 1;j <= 2*i-1;j++)
-		{
-			printf("%d",line++);
+			for(j = 1;j <= 2*i-1;j++)
+			{
+				printf("%d",line++);
+			}
+			printf("\n");
 		}
-		printf("\n");
+	
+		printf("\n=======================\n\n");
 	}
+	
+	return 0;
 }
